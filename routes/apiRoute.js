f95ffd6e9d263de96da6bb09c2e24912a710152b
SpencerFW <srfalorward@ucdavis.edu>
2020-04-07 15:53:48 -0700

    html and api routes js started

---
+++ routes/apiRoute.js
@@ -0,0 +1,46 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var emoji = require("../models/emoji.js");
+
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // Get all chirps
+  app.get("/api/all", function(req, res) {
+
+    // Finding all Chirps, and then returning them to the user as JSON.
+    // Sequelize queries are asynchronous, which helps with perceived speed.
+    // If we want something to be guaranteed to happen after the query, we'll use
+    // the .then function
+    //The empty object is telling it that there are no restrictions and to return all the data
+    emoji.findAll({}).then(function(results) {
+      // results are available to us inside the .then
+      res.json(results);
+    });
+
+  });
+
+  // Add a emoji-gram
+  app.post("/api/new", async function(req, res) {
+
+    console.log("Emoji Data:");
+    console.log(req.body);
+//below is creating the data. similar to INSERT INTO like we were using previously
+    var results = await emoji.create({
+      name: req.body.name,
+      code: req.body.code,
+      category: req.body.category
+    })
+      res.end();
+      // if you did have somehting to return you would do
+      //res.json(results)
+
+  });
+
+};
---
+++ routes/htmlRoute.js
@@ -0,0 +1,33 @@
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var path = require('path')
+
+// Routes
+// =============================================================
+module.exports = function (app) {
+	// Each of the below routes just handles the HTML page that the user gets sent to.
+
+	// index route loads index.html
+	app.get('/', function (req, res) {
+		res.sendFile(path.join(__dirname, '../views/index.html'))
+	})
+
+    // scores route loads scores.html
+	app.get('/scores', function (req, res) {
+		res.sendFile(path.join(__dirname, '../views/scores.html'))
+    })
+
+    // game route loads game.html
+	app.get('/game', function (req, res) {
+		res.sendFile(path.join(__dirname, '../views/game.html'))
+	})
+
+	// index route loads index.html
+	app.get('/index', function (req, res) {
+		res.sendFile(path.join(__dirname, '../views/index.html'))
+	})
+}
