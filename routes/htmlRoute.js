fd40af5e636669bcfa1b1314af884e8a667aa985
SpencerFW <srfalorward@ucdavis.edu>
2020-04-08 11:44:38 -0700

    route files tested

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -4,7 +4,7 @@
 
 // Dependencies
 // =============================================================
-var path = require('path')
+// var path = require('path')
 
 // Routes
 // =============================================================
@@ -12,22 +12,23 @@ module.exports = function (app) {
 	// Each of the below routes just handles the HTML page that the user gets sent to.
 
 	// index route loads index.html
-	app.get('/', function (req, res) {
-		res.sendFile(path.join(__dirname, '../views/index.html'))
+	app.get("/", function (req, res) {
+		// res.sendFile(path.join(__dirname, '../views/index.html'))
+		res.render("index")
 	})
 
-    // scores route loads scores.html
-	app.get('/scores', function (req, res) {
-		res.sendFile(path.join(__dirname, '../views/scores.html'))
-    })
+	// // scores route loads scores.html
+	// app.get('/scores', function (req, res) {
+	// 	res.sendFile(path.join(__dirname, '../views/scores.html'))
+	// })
 
-    // game route loads game.html
-	app.get('/game', function (req, res) {
-		res.sendFile(path.join(__dirname, '../views/game.html'))
-	})
+	// // game route loads game.html
+	// app.get('/game', function (req, res) {
+	// 	res.sendFile(path.join(__dirname, '../views/game.html'))
+	// })
 
-	// index route loads index.html
-	app.get('/index', function (req, res) {
-		res.sendFile(path.join(__dirname, '../views/index.html'))
-	})
+	// // index route loads index.html
+	// app.get('/index', function (req, res) {
+	// 	res.sendFile(path.join(__dirname, '../views/index.html'))
+	// })
 }
