224d04cae243f3fd66a80fac2daa116eaf0e649b
Gurinderp <58455178+Gurinderp@users.noreply.github.com>
2020-04-11 10:23:33 -0700

    html, css, js files (#5)

---
+++ index.js
@@ -0,0 +1,32 @@
+// Start Button
+var startBtn = document.getElementById("start");
+
+// Input
+var inputEl = document.getElementById("input");
+
+// Outputs
+var answerEl = document.getElementById("answerOutput");
+var emojiEl = document.getElementById("emojiOutput");
+
+// Submit
+var submitBtn = document.getElementById("submit");
+
+// Timer
+var timer = document.getElementById("timeRem")
+var time = 25
+
+// Results
+var correct = 0;
+var score = 0;
+
+function startGame(){
+    startBtn.addEventListener("click", () => {
+        
+    })
+}
+
+function badInput(){
+    inputEl.addEventListener("click", function(){
+        
+    })
+}
