657f05e3355a5df4653cc49500b871bbda5ee2e4
Spencer Falor-Ward <srfalorward@ucdavis.edu>
2020-04-15 16:27:46 -0700

    Sfw api html routes (#16)
    * test file
    
    * removed thge public dirctory ignore in .gitignore
    
    * add jQuery to eslintrc
    
    * node version added to package.json
    
    * node start to package.json
    
    * fixed node version
    
    * question population

--- models/emoji.js
+++ models/emoji.js
@@ -2,17 +2,17 @@ module.exports = (sequelize, DataTypes) => {
 	const Emoji = sequelize.define("Emoji", {
 		code: {
 			type: DataTypes.TEXT,
-			allowNull: false,
-			validate: {
-				len: [1],
-			},
+			// allowNull: false,
+			// validate: {
+			// 	len: [1],
+			// },
 		},
 		unicode: {
 			type: DataTypes.TEXT,
-			allowNull: false,
-			validate: {
-				len: [1],
-			},
+			// allowNull: false,
+			// validate: {
+			// 	len: [1],
+			// },
 		},
 		category: DataTypes.TEXT,
 	})
